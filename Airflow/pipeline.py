3720fb37eddbdcffe609aa2beda60963e8f0d83a
Watcharee-S <watcharee.skr@gmail.com>
2022-08-24 17:27:27 +0700

    change disk_size to meet quotas

--- Airflow/pipeline.py
+++ Airflow/pipeline.py
@@ -34,12 +34,12 @@ CLUSTER_CONFIG = {
     "master_config": {
         "num_instances" : 1,
         "machine_type_uri" : "n1-standard-2",
-        "disk_config" : {"boot_disk_type": "pd-standard", "boot_disk_size_gb": 1024},
+        "disk_config" : {"boot_disk_type": "pd-standard", "boot_disk_size_gb": 500},
     },
     "worker_config": {
         "num_instances" : 2,
         "machine_type_uri" : "n1-standard-2",
-        "disk_config" : {"boot_disk_type": "pd-standard", "boot_disk_size_gb": 1024},
+        "disk_config" : {"boot_disk_type": "pd-standard", "boot_disk_size_gb": 500},
     },
 }
 
